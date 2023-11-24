0f43a06ce2b00c717bf106e1332fb665e0cf3dd9
MartinchoCP <martincubide6@gmail.com>
2023-11-23 21:45:05 -0500

    final video #1
    Creacion de la api y creación de estructura de capas con cadena de conexion

--- Ecommerce.Repositorio/DBContext/DbProyectoSpContext.cs
+++ Ecommerce.Repositorio/DBContext/DbProyectoSpContext.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using Ecommerce.Modelo;
 using Microsoft.EntityFrameworkCore;
 
-namespace Ecommerce.Modelo;
+namespace Ecommerce.Repostorio.DBContext;
 
 public partial class DbProyectoSpContext : DbContext
 {
@@ -59,9 +60,7 @@ public partial class DbProyectoSpContext : DbContext
 
     public virtual DbSet<Venta> Venta { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(local); DataBase=DbProyectoSP; Trusted_Connection=True; TrustServerCertificate=True;");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
