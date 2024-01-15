f306fe6af1b80b12cbba422f20fa9b106bb8818a
JuanDavid38 <jdavid3ramirez.999@gmail.com>
2024-01-15 15:51:06 -0500

    Parte #5 y #6 Terminadas
    se realizo las partes 5 y 6 del Ecommerce

---
+++ Ecommerce.WebAssembly/Servicios/Contrato/ICarritoServicio.cs
@@ -0,0 +1,17 @@
+﻿using Ecommerce.DTO;
+
+namespace Ecommerce.WebAssembly.Servicios.Contrato
+{
+    public interface ICarritoServicio
+    {
+
+        event Action MostarItems;
+
+        int CantidadProductos();
+        Task AgregarAlCarrito(CarritoDTO modelo);
+        Task EliminarDeCarrito(int idProducto);
+        Task<List<CarritoDTO>> DevolverCarrito();
+        Task LimpiarCarrito();
+
+    }
+}
